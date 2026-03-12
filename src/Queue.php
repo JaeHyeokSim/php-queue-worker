cb8a5810811afe7e01992951a07a5d04bc92bdad
JaeHyeokSim <01088825238sjh@gmail.com>
2026-03-12 11:43:24 +0900

    Add file locking to queue pop operation

--- src/Queue.php
+++ src/Queue.php
@@ -1,10 +1,12 @@
 <?php
 
-class Queue {
+class Queue
+{
 
 	private $file;
 
-	public function __construct($file) {
+	public function __construct($file)
+	{
 		$this->file = $file;
 
 		if (!file_exists($file)) {
@@ -12,26 +14,58 @@ class Queue {
 		}
 	}
 
-	public function push($job) {
+	public function push($job)
+	{
 
-		$queue = json_decode(file_get_contents($this->file), true);
+		$fp = fopen($this->file, "c+");
+
+		flock($fp, LOCK_EX);
+
+		$queue = json_decode(stream_get_contents($fp), true);
+
+		if (!$queue) {
+			$queue = [];
+		}
 
 		$queue[] = $job;
 
-		file_put_contents($this->file, json_encode($queue));
+		ftruncate($fp, 0);
+		rewind($fp);
+
+		fwrite($fp, json_encode($queue));
+
+		flock($fp, LOCK_UN);
+
+		fclose($fp);
 	}
 
-	public function pop() {
+	public function pop()
+	{
 
-		$queue = json_decode(file_get_contents($this->file), true);
+		$fp = fopen($this->file, "c+");
 
-		if (empty($queue)) {
+		flock($fp, LOCK_EX);
+
+		$queue = json_decode(stream_get_contents($fp), true);
+
+		if (!$queue || empty($queue)) {
+
+			flock($fp, LOCK_UN);
+			fclose($fp);
+
 			return null;
 		}
 
 		$job = array_shift($queue);
 
-		file_put_contents($this->file, json_encode($queue));
+		ftruncate($fp, 0);
+		rewind($fp);
+
+		fwrite($fp, json_encode($queue));
+
+		flock($fp, LOCK_UN);
+
+		fclose($fp);
 
 		return $job;
 	}
